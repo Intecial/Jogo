89e37a09c27955d735279b03cc1dbc15317c61ce
Jessica Lim <jlim0154@student.monash.edu>
2021-11-20 23:44:33 +0700

    decorate homepage and add designs

--- jogo-app/src/component/heroSection.jsx
+++ jogo-app/src/component/heroSection.jsx
@@ -12,8 +12,22 @@ const HeroSelection = () =>
             </div>
         </header>
 
-    
-        <video src ={ Nature } muted loop autoPlay className="vid-nature"></video>
+        <div class="showcase">
+            <video src ={ Nature } muted loop autoPlay></video>
+                <div class="title-text">
+                    <h2 class="bigger-title">protect our home</h2>
+                    <h3 class="bigger-title">and our loved ones...</h3>
+                    <h4 class="subtitle">Earth Day Sale All Items 50% off !!!</h4>
+                    <button href="" class="why-btn"><p>why</p></button> <button href="" class="donate-btn"><p>donate</p></button>
+                </div>
+        </div>
+
+        <footer>
+            <div>
+                
+            </div>
+        </footer>
+        
     </>);
 }
  
