cf88e62d0eaec958571c33a6c8da2f5ce5446d9b
Intecial <ljuliano30@gmail.com>
2021-11-20 12:42:49 +0700

    Adding a Navigation Bar Place holder

--- jogo-app/src/index.js
+++ jogo-app/src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.css';
+import NavBar from './component/navbar'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <NavBar />
   </React.StrictMode>,
   document.getElementById('root')
 );
